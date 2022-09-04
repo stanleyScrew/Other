e0479d11946142158d5051a16c02bec22690a994
stanleyScrew <igor.grishchenko74@gmail.com>
2022-09-04 10:50:03 +0300

    Fix040922

--- src/Main.java
+++ src/Main.java
@@ -61,7 +61,7 @@ public class Main {
                     System.out.println("\t" + products[i] + " " + (doBonus ? counts[i] + 1 : counts[i]) + " шт. за " + (prices[i] * (counts[i] / 3 * 2 + counts[i] % 3)) + " руб. (распродажа!)");
                     sum1 += prices[i] * (counts[i] / 3 * 2 + counts[i] % 3);
                 } else {
-                    System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * counts[i]) + " руб.");
+                    System.out.println("\t" + products[i] + " " + (counts[i] * 2) + " шт. за " + (prices[i] * counts[i]) + " руб.");
                     sum2 += prices[i] * counts[i];
                 }
             }
